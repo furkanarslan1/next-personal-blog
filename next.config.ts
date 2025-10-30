4d9c17ee90514921ddc5bfa071e83b116037edf7
Furkan Arslan <furkanarslandev@gmail.com>
2025-10-30 13:57:09 +0300

    Revert "feat:add authSecret_next_config"
    This reverts commit 94608e63d3d8e4ece313eb44065d740f252280c8.

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    AUTH_SECRET: process.env.AUTH_SECRET,
-  },
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com",
+        hostname: "res.cloudinary.com", // 👈 Cloudinary domaini eklendi
       },
     ],
   },
