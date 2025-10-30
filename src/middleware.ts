62dab6dd7a94aeabf6abc644b3f79175126008ed
Furkan Arslan <furkanarslandev@gmail.com>
2025-10-30 18:50:33 +0300

    refactor:middleware_auth_problem

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,6 +18,7 @@
 //   matcher: ["/admin/:path*"],
 // };
 
+// ***************  asıl kullandığım************
 // import { NextRequest, NextResponse } from "next/server";
 // import { getToken } from "next-auth/jwt";
 
@@ -40,34 +41,68 @@
 //   // İf user is Admin
 //   return NextResponse.next();
 // }
+// *************************************************************************
 
 // export const config = {
 //   matcher: ["/admin/:path*"],
 // };
 
-import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+// import { NextRequest, NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
+
+// export async function middleware(request: NextRequest) {
+//   const token = await getToken({
+//     req: request,
+//     // secret: process.env.AUTH_SECRET,
+//   });
+
+//   // Not logged in user
+//   if (!token) {
+//     return NextResponse.redirect(new URL("/login", request.nextUrl.origin));
+//   }
+
+//   // user isn't admin
+//   if (typeof token.role !== "string" || token.role !== "ADMIN") {
+//     return NextResponse.redirect(new URL("/", request.nextUrl.origin));
+//   }
+
+//   // İf user is Admin
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/admin/:path*"],
+// };
+
+// src/middleware.ts
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { auth } from "./auth"; // <-- Doğru import yolu
+
+export const config = {
+  matcher: ["/admin/:path*", "/login"],
+};
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({
-    req: request,
-    // secret: process.env.AUTH_SECRET,
-  });
+  const session = await auth();
 
-  // Not logged in user
-  if (!token) {
+  const isAdminRoute = request.nextUrl.pathname.startsWith("/admin");
+  const isLoginRoute = request.nextUrl.pathname === "/login";
+
+  if (!session && isAdminRoute) {
     return NextResponse.redirect(new URL("/login", request.nextUrl.origin));
   }
 
-  // user isn't admin
-  if (typeof token.role !== "string" || token.role !== "ADMIN") {
+  if (session && session.user.role !== "ADMIN" && isAdminRoute) {
     return NextResponse.redirect(new URL("/", request.nextUrl.origin));
   }
 
-  // İf user is Admin
+  if (session && isLoginRoute) {
+    return NextResponse.redirect(
+      new URL("/admin/blogs", request.nextUrl.origin)
+    );
+  }
+
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/admin/:path*"],
-};
