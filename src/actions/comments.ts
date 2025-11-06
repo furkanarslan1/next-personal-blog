a62f28166c79316bd49017c64265817ea38a2f7a
Furkan Arslan <furkanarslandev@gmail.com>
2025-11-06 12:50:47 +0300

    feat:add movies_comment_action

--- src/actions/comments.ts
+++ src/actions/comments.ts
@@ -74,3 +74,60 @@ export async function getCommentByPostIdAction(postId: string) {
     return [];
   }
 }
+
+//  *************************** MOVIE COMMENTS **********************************************************************
+
+export async function addMovieComment(movieId: string, content: string) {
+  const session = await auth();
+  if (!session || !session.user) {
+    return { success: false, message: "You need to log in." };
+  }
+
+  const userId = session.user.id;
+
+  const movie = await prisma.movie.findUnique({ where: { id: movieId } });
+
+  if (!movie) {
+    return { success: false, message: "Movie post not found." };
+  }
+
+  const existingComment = await prisma.comment.findFirst({
+    where: { movieId, authorId: userId },
+  });
+
+  if (existingComment) {
+    return {
+      success: false,
+      message: "You have already commented on this post.",
+    };
+  }
+
+  await prisma.comment.create({
+    data: { content, movieId, authorId: userId },
+  });
+
+  revalidatePath(`/movies/${movie.genres[0]}/${movie.slug}`);
+  return { success: true, message: "Comment added successfully." };
+}
+
+export async function getCommentByMovieIdAction(movieId: string) {
+  try {
+    const comments = await prisma.comment.findMany({
+      where: { movieId },
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true,
+            image: true,
+          },
+        },
+      },
+      orderBy: { createdAt: "desc" },
+    });
+    return comments;
+  } catch (error) {
+    console.error("Failed to fetch comments:", error);
+    return [];
+  }
+}
